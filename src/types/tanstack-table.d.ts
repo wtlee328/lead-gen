0403e3b7979951ca1088ab458ed17c5ab9f7e267
wtlee328 <36742847+wtlee328@users.noreply.github.com>
2025-05-20 19:52:57 -0400

    Delete tanstack-table.d.ts

--- src/types/tanstack-table.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/tanstack-table.d.ts
-import '@tanstack/vue-table'; // Important to ensure this is treated as an augmentation
-
-declare module "@tanstack/vue-table" {
-  interface ColumnMeta<TData, TValue> {
-    style?: Record<string, string>;
-  }
-}
